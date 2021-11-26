998db8b04e5c00ee1133a04b107983d339800c3a
ayush <S.A.S.patandin@student.tudelft.nl>
2021-11-26 02:20:59 +0100

    Adjusted chart to better format

--- scripts/d3map.js
+++ scripts/d3map.js
@@ -275,9 +275,9 @@ function drawChart(data) {
     if (d3.select("svg.chart")) d3.select("svg.chart").remove();
 
     // set the dimensions and margins of the graph
-    const margin = {top: 20, right: 30, bottom: 40, left: 90},
-    width = 1100 - margin.left - margin.right,
-    height = municipalityMode ? 3500 : 300 - margin.top - margin.bottom;
+    const margin = {top: 50, right: 30, bottom: 50, left: 150},
+    width = 800 - margin.left - margin.right,
+    height = (municipalityMode ? 600 : 250) - margin.top - margin.bottom;
 
     // append the svg object to the body of the page
     const svg_chart = d3.select("#horizontal-chart")
@@ -289,15 +289,25 @@ function drawChart(data) {
     .attr("transform",
         "translate(" + margin.left + "," + margin.top + ")");
     
-
-    const max = d3.max(data, function (d) { 
-        return getCatWithUndefCheck(d)
-    });
+    const top30Data = data.slice()
+        .sort((a, b) => d3.descending(getCatWithUndefCheck(a), getCatWithUndefCheck(b)))
+        .filter(function(d, i) { return i >= 0 && i < 30; })
+    const max = d3.max(top30Data, function (d) { 
+            return getCatWithUndefCheck(d)
+        });
     // Add X axis
     const x = d3.scaleLinear()
     .domain([0, max])
     .range([ 0, width]);
+    svg_chart.append("text")
+    .attr("x", (width / 2))             
+    .attr("y", 0 - (margin.top / 2))
+    .attr("text-anchor", "middle")  
+    .style("font-size", "30px") 
+    .style("font-weight", "bold")  
+    .text("Locations sorted by top amount of cases");
     svg_chart.append("g")
+    .attr("class", "axis")
     .attr("transform", "translate(0," + height + ")")
     .call(d3.axisBottom(x))
     .selectAll("text")
@@ -307,21 +317,35 @@ function drawChart(data) {
     // Y axis
     const y = d3.scaleBand()
     .range([ 0, height ])
-    .domain(data.map(function(d) { return getMode(d); }))
+    .domain(top30Data.map(function(d) { return getMode(d); }))
     .padding(.1);
     svg_chart.append("g")
+    .attr("class", "axis")
     .call(d3.axisLeft(y))
 
+    const defs = svg_chart.append('defs');
+    const bgGradient = defs
+    .append('linearGradient')
+    .attr('id', 'bg-gradient')
+    // .attr('gradientTransform', 'rotate(90)');
+    bgGradient
+    .append('stop')
+    .attr('stop-color', '#F2C66B')
+    .attr('offset', '0%');
+    bgGradient
+    .append('stop')
+    .attr('stop-color', '#D13D73')
+    .attr('offset', '100%');
     //Bars
     svg_chart.selectAll("myRect")
-    .data(data)
+    .data(top30Data)
     .enter()
     .append("rect")
     .attr("x", x(0) )
     .attr("y", function(d) { return y(getMode(d)); })
     .attr("width", function(d) { return x(getCatWithUndefCheck(d)); })
     .attr("height", y.bandwidth() )
-    .attr("fill", "#69b3a2")
+    .attr("fill", "url(#bg-gradient)")
 }
 
 
