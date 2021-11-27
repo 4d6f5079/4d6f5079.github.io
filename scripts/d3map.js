4b975f962d95a01356c5474a001b08aa5ab0ac67
ayush <S.A.S.patandin@student.tudelft.nl>
2021-11-27 01:10:33 +0100

    no init legend on the end

--- scripts/d3map.js
+++ scripts/d3map.js
@@ -293,6 +293,9 @@ function drawMap(data) {
     if (d3.select(`#${gElemId}`)) d3.select(`#${gElemId}`).remove();
     if (d3.select(`g${legendClass}`)) d3.select(`g${legendClass}`).remove();
     
+    // initialize the legend for the map.
+    initLegend();
+    
     // Append the g element where the paths will be stored and reset zoom if active.
     if (zoomActive) reset(); 
 
@@ -310,8 +313,6 @@ function drawMap(data) {
     .on("click", mouseclicked)
     .call(d3.helper.tooltip(tooltipText, true));
 
-    // initialize the legend for the map.
-    initLegend();
 }
 
 
