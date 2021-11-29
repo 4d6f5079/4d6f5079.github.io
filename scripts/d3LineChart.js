02195c3fe2598e7ba8a6e99ac89475ee036559c1
Adam <adam_school@outlook.com>
2021-11-29 13:25:59 +0100

    Line not visible in line chart, axis are displayed well. Added console log for debugging.

--- scripts/d3LineChart.js
+++ scripts/d3LineChart.js
@@ -8,18 +8,21 @@ function makeTitle() {
 function drawLineChart(allCovidData) {
   if (!selectedPlace) return;
 
-  singlePlaceData = allCovidData.filter(obj => {
+  let singlePlaceData = allCovidData.filter(obj => {
     if(municipalityMode) {
       return obj.Municipality_name === selectedPlace;
     }
     return obj.Province === selectedPlace;
   });
 
+
   console.log("Selected catagory is ", selectedCategory);
   console.log("Selected place is ", selectedPlace);
   let margin = {top: 15, right: 35, bottom: 20, left: 35},
       width = 450 - margin.left - margin.right,
       height = 400 - margin.top - margin.bottom;
+
+  let  parseDate = d3.timeFormat("%Y-%m-%d");
 
   let svg = d3.select("#line-chart")
       .append("svg")
@@ -30,7 +33,7 @@ function drawLineChart(allCovidData) {
 
   // Making x and y axis to append to add to svg
   let x = d3.scaleTime()
-    .domain(d3.extent(singlePlaceData, d => new Date(d.Date_of_report.substring(0,10))))
+    .domain(d3.extent(singlePlaceData, d => new Date(d.Date_of_report.substring(0,10)))) // Taking only the date with substring
     .range([0, width]);
 
   let y = d3.scaleLinear()
@@ -50,11 +53,17 @@ function drawLineChart(allCovidData) {
      .attr("fill", "none")
      .attr("stroke", "steelblue")
      .attr("stroke-width", 1.5)
-     .attr("d", d3.line()
-      .x(d => x(new Date(d.Date_of_report.substring(0,10))))
-      .y(d => y(+d.value)));
+     .attr("d", d3.line().curve(d3.curveLinear)
+      .x(d => { console.log("X value:", x(new Date(d.Date_of_report.substring(0,10))));
+        x(new Date(d.Date_of_report.substring(0,10)))
+      })
+      .y(d => {
+        console.log("Y value:", y(+d.Deceased));
+        y(+d.Deceased)
+      }));
   // selectedDate = document.getElementById("selectedDate").value;
 
   // const data = municipalityCheck();
+
 }
 
