2d9393d63bde0a0d048dce328b934abb047c9861
4d6f5079 <mohamed.5509@live.nl>
2021-12-15 17:04:03 +0100

    fixed python script format to generate correct format needed

--- preprocess_by_date_covid.py
+++ preprocess_by_date_covid.py
@@ -1,29 +1,44 @@
-from csv import DictReader
-import json
+from csv import DictReader, DictWriter
 
 FILE_NAME = "data/COVID-19_aantallen_gemeente_cumulatief.csv"
-RES_FILEPATH = "data/covidDataByDate.json"
+RES_FILEPATH = "data/covidDataByDate.csv"
 
-resDict = {}
+resList = []
+headers = None
+
+provinceCounters = {
+    "Groningen": [0, 0, 0],
+    "Friesland": [0, 0, 0],
+    "Drenthe": [0, 0, 0],
+    "Flevoland": [0, 0, 0],
+    "Overijssel": [0, 0, 0],
+    "Gelderland": [0, 0, 0],
+    "Utrecht": [0, 0, 0],
+    "Noord-Holland": [0, 0, 0],
+    "Zuid-Holland": [0, 0, 0],
+    "Zeeland": [0, 0, 0],
+    "Noord-Brabant": [0, 0, 0],
+    "Limburg": [0, 0, 0],
+}
 
 
 def main():
-    with open(RES_FILEPATH, "w", encoding="utf-8") as r:
+    global provinceCounters
+    
+    with open(RES_FILEPATH, "w", newline="") as r:
         with open(FILE_NAME) as f:
             csvReader = DictReader(f, delimiter=";")
+            headers = csvReader.fieldnames
+            prevDate = None
             for row in csvReader:
-                date = row["Date_of_report"]
-                province = row["Province"]
-                totalReported = int(row["Total_reported"])
-                hospitalAdmissions = int(row["Hospital_admission"])
-                deceased = int(row["Deceased"])
-                municipalityCode = row["Municipality_code"]
+                currDate = row["Date_of_report"]
 
-                if not municipalityCode:
-                    continue
+                if prevDate is None:
+                    prevDate = currDate
 
-                if not resDict.get(date, False):
-                    resDict[date] = {
+                if prevDate != currDate:
+                    prevDate = currDate
+                    provinceCounters = {
                         "Groningen": [0, 0, 0],
                         "Friesland": [0, 0, 0],
                         "Drenthe": [0, 0, 0],
@@ -38,13 +53,112 @@ def main():
                         "Limburg": [0, 0, 0],
                     }
 
-                currRow = resDict[date][province]
-                currRow[0] += totalReported
-                currRow[1] += hospitalAdmissions
-                currRow[2] += deceased
+                province = row["Province"]
+                totalReported = int(row["Total_reported"])
+                hospitalAdmissions = int(row["Hospital_admission"])
+                deceased = int(row["Deceased"])
+                municipalityCode = row["Municipality_code"]
 
-        json.dump(resDict, r, ensure_ascii=False, indent=4)
+                if not municipalityCode:
+                    newRow = row
+                    newRow["Total_reported"] = str(provinceCounters[province][0])
+                    newRow["Hospital_admission"] = str(provinceCounters[province][1])
+                    newRow["Deceased"] = str(provinceCounters[province][2])
+                    resList.append(newRow)
+                else:
+                    resList.append(row)
+                    provinceCounters[province][0] += totalReported
+                    provinceCounters[province][1] += hospitalAdmissions
+                    provinceCounters[province][2] += deceased
+
+        dw = DictWriter(r, delimiter=";", fieldnames=headers)
+
+        prev_headers = headers
+        headers = {}
+        for h in prev_headers:
+            headers[h] = h
+
+        dw.writerow(headers)
+
+        for rw in resList:
+            dw.writerow(rw)
 
 
 if __name__ == "__main__":
     main()
+
+
+# resDict[date] = [
+#                         {
+#                             "Province": "Groningen",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Friesland",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Drenthe",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Flevoland",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Overijssel",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Gelderland",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Utrecht",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Noord-Holland",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Zuid-Holland",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Zeeland",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Noord-Brabant",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                         {
+#                             "Province": "Limburg",
+#                             "Total_reported": 0,
+#                             "Hospital_admission": 0,
+#                             "Deceased": 0,
+#                         },
+#                     ]
